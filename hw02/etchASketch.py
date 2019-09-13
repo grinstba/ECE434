bdf5e4cf9c764d22a3a7ea8e4e3d5c2d6490ead8
Demo User <debian@beaglebone.localdomain>
2019-09-13 04:39:47 +0000

    Added curses and keyboard input rather than commands

--- hw02/etchASketch.py
+++ hw02/etchASketch.py
@@ -1,5 +1,61 @@
 #!/usr/bin/env python3
 import random
+import curses
+
+def main(stdscr):
+    
+    # Generate a random starting location
+    xPos = random.randint(0, width - 1)
+    yPos = random.randint(0, height - 1)
+
+    while True:
+        showScreen(stdscr, width, height)
+        pressedKey = stdscr.getch()
+        
+        if pressedKey == curses.KEY_UP:
+            yPos-= 1
+            if (yPos <= 0):
+                yPos = 0
+            grid[yPos][xPos] = 'X'
+        elif pressedKey == curses.KEY_DOWN:
+            yPos+= 1
+            if (yPos >= height):
+                yPos = height - 1
+            grid[yPos][xPos] = 'X'
+        elif pressedKey == curses.KEY_RIGHT:
+            xPos+= 1
+            if (xPos >= width):
+                xPos = width - 1
+            grid[yPos][xPos] = 'X'
+        elif pressedKey == curses.KEY_LEFT:
+            xPos-= 1
+            if (xPos <= 0):
+                xPos = 0
+            grid[yPos][xPos] = 'X'
+        elif pressedKey == ord(' '):
+            for k in range(len(grid)):
+                for i in range(len(grid[k])):
+                    grid[k][i] = ' '
+    
+def showScreen(stdscr, width, height):
+    # Clear the screen
+    stdscr.clear()
+    
+    # Print the top line
+    for k in range(width):
+        stdscr.addstr(0, 2 + 3*k, ' ' + str(k))
+        
+     # Print each grid row
+    for k in range(len(grid)):
+        stdscr.addstr(k+1, 0, str(k))
+        for i in range(len(grid[k])):
+            stdscr.addstr(k+1, 3 + i*3, str(grid[k][i]))
+            
+    stdscr.addstr(height+1, 0, 'Enter command (up, down, left, right, clear): ')
+    
+    # Show the screen
+    stdscr.refresh()
+
 
 # Get board dimensions
 width = int(input('Enter board width: '))
@@ -8,53 +64,5 @@ height = int(input('Enter board height: '))
 # Create the grid
 grid = [[' ' for i in range(width)] for j in range(height)]
 
-# Generate a random starting location
-xPos = random.randint(0, width - 1)
-yPos = random.randint(0, height - 1)
 
-# Main loop to use the etch-a-sketch
-while (True):
-    # Print the top line
-    print('  ', end='')
-    for z in range(len(grid[0])):
-        print(' ' + str(z), end='')
-    
-    print('')
-    
-    # Print each grid row
-    for k in range(len(grid)):
-        print(str(k) + ':', end='')
-        for i in range(len(grid[k])):
-            print(' ' + str(grid[k][i]), end='')
-    
-        print('')
-        
-    # Ask for a user command
-    command = input('Enter command (up, down, left, right, clear): ')
-    
-    if (command == 'right'):
-        xPos+= 1
-        if (xPos >= width):
-            xPos = width - 1
-        grid[yPos][xPos] = 'X'
-    elif (command == 'left'):
-        xPos-= 1
-        if (xPos <= 0):
-            xPos = 0
-        grid[yPos][xPos] = 'X'
-    elif (command == 'down'):
-        yPos+= 1
-        if (yPos >= height):
-            yPos = height - 1
-        grid[yPos][xPos] = 'X'
-    elif (command == 'up'):
-        yPos-= 1
-        if (yPos <= 0):
-            yPos = 0
-        grid[yPos][xPos] = 'X'
-    elif (command == 'clear'):
-        for k in range(len(grid)):
-            for i in range(len(grid[k])):
-                grid[k][i] = ' '
-    else:
-        print('Invalid Command')
+curses.wrapper(main)
