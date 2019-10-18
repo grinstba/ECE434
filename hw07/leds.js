790f61876c9612afc4b7c8f3b170eaf76a3aacf8
Brock <grinstba@rose-hulman.edu>
2019-10-18 15:46:46 +0000

    The slider is working on the 4.14 Kernel

--- hw07/leds.js
+++ hw07/leds.js
@@ -5,28 +5,25 @@ const b = require('bonescript');
 const util = require('util');
 
 const LED0 = 'USR3';
-const button = 'P9_25';
+const LED1 = 'P9_14';
 b.pinMode(LED0, b.OUTPUT);
-b.pinMode(button, b.INPUT);
+b.pinMode(LED1, b.ANALOG_OUTPUT);
 
 const AUTH = 'xKOrQO_5UjP1IVxbF98LvlW7itaLGUCZ';
-
 
 var blynk = new Blynk.Blynk(AUTH);
 
 var v0 = new blynk.VirtualPin(0);
-var v10 = new blynk.WidgetLED(10);
+var v1 = new blynk.VirtualPin(1);
+// var v10 = new blynk.WidgetLED(10);
 
 v0.on('write', function(param) {
     console.log('V0:', param[0]);
     b.digitalWrite(LED0, param[0]);
 });
 
-v10.setValue(0);    // Initiallly off
-
-b.attachInterrupt(button, toggle, b.CHANGE);
-
-function toggle(x) {
-    console.log("V10: ", x.value);
-    x.value ? v10.turnOff() : v10.turnOn();
-}
+v1.on('write', function(param) {
+    var value = param[0]/1023;
+    console.log('V1 this file:', value);
+    b.analogWrite(LED1, value);
+});
